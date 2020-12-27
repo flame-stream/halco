7a37da5a26dc4229a5b9ef16198d4082761705f6
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2020-12-27 19:34:03 +0300

    Gen written

--- calcohs/src/Calco/GraphGen.hs
+++ calcohs/src/Calco/GraphGen.hs
@@ -3,12 +3,15 @@
 
 module Calco.GraphGen where
 
-import           Control.Monad (guard)
-import           Data.Function ((&))
-import           Data.Map      (Map, (!))
-import qualified Data.Map      as Map
-import           Data.Set      (Set)
-import qualified Data.Set      as Set
+import           Control.Monad             (guard)
+import qualified Control.Monad.State       as StateM
+import           Control.Monad.Trans.Class (MonadTrans)
+import           Data.Function             ((&))
+import           Data.Map                  (Map, (!))
+import qualified Data.Map                  as Map
+import           Data.Set                  (Set)
+import qualified Data.Set                  as Set
+import           ListT
 
 import           Calco.CGraph
 import           Calco.Conts
@@ -21,10 +24,11 @@ genGraphs :: ContContext a p i o => CGraph i o -> [Graph]
 genGraphs (e@(Env m), s) =
   let ss = zip [1..] $ streams e
       consts = (Const <$>) <$> ss
-      bigGraph = graph $ consts ++ genFromSources 10 e
+      g = genFromSources 10 e
         (Set.fromList $ tfms e)
-        (fst $ last ss)
         (fmap (toState . stream e) <$> ss)
+      tidMax = fst $ last ss
+      bigGraph = graph $ consts ++ StateM.evalState (toList g) tidMax
    in map (bigGraph `cut`) $ semanticsTids bigGraph s
   where
     semanticsTids :: Graph -> Semantics -> [Set TermId]
@@ -34,27 +38,31 @@ genGraphs (e@(Env m), s) =
 
 genFromSources :: ContContext a p i o
                => Int -> Env i o -> Set NodeName
-               -> TermId -> [(TermId, State a p)]
-               -> [(TermId, Term)]  -- TODO monad transformer with Writer to store tidMax
-genFromSources 0 _ _ _ _ = []
-genFromSources depth e@(Env m) nns tidMax sources = do
-  nn <- Set.toList nns
+               -> [(TermId, State a p)]
+               -> ListT (StateM.State TermId) (TermId, Term)
+genFromSources 0 _ _ _ = ListT $ pure Nothing
+genFromSources depth e@(Env m) nns sources = do
+  nn <- fromFoldable nns
   m ! nn & \case
-    Stream _ -> error "Streams should be in sources"
+    Stream _ -> error "Streams should be only in sources"
     Tfm1 i o -> do
-      (tid, state) <- sources
+      (tid, state) <- fromFoldable sources
       guard $ state `match` i
-      (undefined, App1 nn tid) : genFromSources (depth - 1) e
+      updateTid
+      tid' <- liftLT StateM.get
+      (tid', App1 nn tid) `cons` genFromSources (depth - 1) e
         (nn `Set.delete` nns)
-        undefined
-        ((undefined, state `update` o) : sources)
+        ((tid', state `update` o) : sources)
     Tfm2 i1 i2 o -> do
-      (tid1, state1) <- sources
-      (tid2, state2) <- sources
+      (tid1, state1) <- fromFoldable sources
+      (tid2, state2) <- fromFoldable sources
       guard $ tid1 /= tid2
             && state1 `match` i1
             && state2 `match` i2
-      (undefined, App2 nn tid1 tid2) : genFromSources (depth - 1) e
+      updateTid
+      tid' <- liftLT StateM.get
+      (tid', App2 nn tid1 tid2) `cons` genFromSources (depth - 1) e
         (nn `Set.delete` nns)
-        undefined
-        ((undefined , state1 `update` o) : sources)
+        ((tid' , state1 `update` o) : sources)
+  where
+    updateTid = liftLT $ StateM.modify (+1)
