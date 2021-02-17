b010b8d5d0573fa2b47f15bceac34469d5c0bf3e
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-02-18 01:25:03 +0300

    Add docs

--- calcohs/src/Calco/Beam.hs
+++ calcohs/src/Calco/Beam.hs
@@ -6,7 +6,7 @@ import           Data.Map                 (Map)
 import qualified Data.Map                 as Map
 
 import           Calco.Defs               (Attr)
-import           Calco.Utils.Data.Functor (fmap2)
+import           Calco.Utils.Data.Functor (fmap2, (<$$>))
 import           Calco.Utils.Data.Map     (insertOrApply)
 
 type Stream e = [e]
@@ -19,31 +19,35 @@ groupBy f = Map.toList . foldr g Map.empty
   where
     g e = insertOrApply (f e) [e] (e :)
 
-groupBy' :: (Ord k, Ord k') => Map k (e -> k') -> [e] -> [(Map k k', [e])]
+-- Group by a different number of named properties.
+groupBy' :: (Ord k, Ord k') => Map k (e -> k') -> Stream e -> [(Map k k', [e])]
 groupBy' ks = Map.toList . foldr f Map.empty
   where
     ap e = fmap ($ e) ks
     f e = insertOrApply (ap e) [e] (e :)
 
-coGroupByKey :: Ord k => (k1, [(k, e1)]) -> (k2, [(k, e2)])
-             -> [(k, ( (k1, [e1])
-                     , (k2, [e2]) ))]
-coGroupByKey (k1, es1) (k2, es2) =
+-- Apply relational join to two streams.
+-- k1, k2 - names of the streams.
+-- k - key for join.
+coGroupByKey :: Ord k => (k1, [(k, v1)]) -> (k2, [(k, v2)])
+             -> [(k, ( (k1, [v1])
+                     , (k2, [v2]) ))]
+coGroupByKey (k1, vs1) (k2, vs2) =
   map from $ groupBy fst
-    [(k, (e1, e2))
-      | (k , e1) <- es1
-      , (k', e2) <- es2
+    [(k, (v1, v2))
+      | (k , v1) <- vs1
+      , (k', v2) <- vs2
       , k == k']
   where
-    from (k, es) = (k, ( (k1, map (fst . snd) es)
-                       , (k2, map (snd . snd) es) ))
+    from (k, es) = (k, ( (k1, fst . snd <$> es)
+                       , (k2, snd . snd <$> es) ))
 
-combinePerKey :: (Ord k, Semigroup e) => [(k, e)] -> [(k, e)]
+combinePerKey :: (Ord k, Semigroup v) => [(k, v)] -> [(k, v)]
 combinePerKey = Map.toList . foldr f Map.empty
   where
     f (k, e) = insertOrApply k e (e <>)
 
-combineValues :: (Ord k, Monoid e) => [(k, [e])] -> [(k, e)]
+combineValues :: (Ord k, Monoid v) => [(k, [v])] -> [(k, v)]
 combineValues = combinePerKey . fmap2 mconcat
 
 flatten :: [Stream e] -> Stream e
