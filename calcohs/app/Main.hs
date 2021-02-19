3f7ccb77d5b82f2f82dad4a5ce69b0a6e1a76ee7
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-02-19 17:12:42 +0300

    Print graphs

--- calcohs/app/Main.hs
+++ calcohs/app/Main.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import           Control.Monad            (forM_)
+
 import           Calco.Examples.Pets.Base
+import           Calco.Graph              (graph2Dot)
 import           Calco.GraphGen.Fast
 
 main :: IO ()
-main = putStrLn $ "Graphs:\n" <> show (genGraphs cgraph)
+main = forM_ (zip [1..] $ genGraphs cgraph) $ \(i :: Integer, g) ->
+  putStrLn $ graph2Dot g (show i)
